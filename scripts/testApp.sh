c274d98c572ab65e3ff90fac169560565357d4e2
Gilbert Kwan <gkwan@ca.ibm.com>
2024-10-29 14:22:35 -0400

    temporarily fix the test
    Signed-off-by: Gilbert Kwan <gkwan@ca.ibm.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -7,26 +7,12 @@ set -euxo pipefail
 ##
 ##############################################################################
 
-mvn -version
-
 mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -ntp -q clean package
-echo unit test
-mvn test
-mvn -ntp -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -ntp -q liberty:create liberty:install-feature liberty:deploy
+    -ntp -q clean package liberty:create liberty:install-feature liberty:deploy
 
 mvn -ntp liberty:start
-
-# need to re-enable the integration-test 
-#mvn -Dhttp.keepAlive=false \
-#    -Dmaven.wagon.http.pool=false \
-#    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-#    -ntp failsafe:integration-test
 
 # check the messages.log
 # check the url
